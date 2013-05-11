41ff279b2f94b78059fbac893d599d7928d849db
naoya <mine@mine-studio.com>
2013-05-12 04:58:18 +0900

    Update 0.1.0

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_open_search do
   name 'Redmine Open Search plugin'
   author 'Mine02C4'
   description 'This is a plugin for Redmine to add support for OpenSearch.'
-  version '0.0.1'
+  version '0.1.0'
   url 'https://github.com/Mine02C4/redmine_open_search'
   author_url 'http://blog.mine-studio.com/'
 end
